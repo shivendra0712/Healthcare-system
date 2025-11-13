b5fb8f58b001007e4302ed502bf0771bfea00349
Shivendra Patel <patelshivendra817@gmail.com>
2025-11-13 10:30:18 +0530

    updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-   origin: "http://localhost:5173",
+   origin: process.env.FRONTEND_URL,
   credentials: true,
 //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 //   allowedHeaders: ['Content-Type', 'Authorization']
